aa217bf80ee670e4023d6ada2d9b0cb29b10611d
Robson Oliveira dos Santos <robsonos@outlook.com>
2019-02-28 17:21:38 -0300

    Fixed splashscreen and statusbar

--- electron/main.js
+++ electron/main.js
@@ -19,7 +19,7 @@ function createWindow() {
 
   // and load the index.html of the app.
   const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, '/../www/index.html'),
+    pathname: path.join(__dirname, '../www/index.html'),
     protocol: 'file:',
     slashes: true
   });
