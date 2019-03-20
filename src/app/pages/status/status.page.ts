0267229138bd69970c70e08b9bcd9d8d7f5d72d6
Robson Oliveira dos Santos <robsonos@outlook.com>
2019-03-19 21:44:46 -0300

    Added send and received data to status page

--- src/app/pages/status/status.page.ts
+++ src/app/pages/status/status.page.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, NgZone } from '@angular/core';
-import { ToastController, AlertController, LoadingController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { Subscription } from 'rxjs';
 import { SerialPortService } from 'src/app/providers/serial-port/serial-port.service';
 import { DataLoggerService } from 'src/app/providers/data-logger/data-logger.service';
-import { Data } from 'src/app/interfaces/data';
 import { SerialDevice } from 'src/app/interfaces/serial-device';
 
 @Component({
@@ -15,16 +14,14 @@ import { SerialDevice } from 'src/app/interfaces/serial-device';
 export class StatusPage implements OnInit {
   dataSubscription: Subscription;
   serialDevice: SerialDevice;
-  data: Data;
   date: Date;
-  loading: any;
+  sentData: any;
+  receivedData: any;
 
   constructor(
     public serialPortService: SerialPortService,
     public dataLoggerService: DataLoggerService,
     public toastController: ToastController,
-    public alertController: AlertController,
-    public loadingController: LoadingController,
     private storage: Storage,
     public zone: NgZone
   ) {}
@@ -54,24 +51,19 @@ export class StatusPage implements OnInit {
     }
 
     // Start listening to stream
-    this.dataSubscription = this.serialPortService.dataStream().subscribe(data => {
-      // FIXME: is zone needed?
-      this.zone.run(async () => {
-        let parsedData: Data;
+    this.dataSubscription = this.serialPortService.dataStream().subscribe(receivedData => {
+      this.zone.run(() => {
+        // this.receivedData = receivedData;
+        // this.date = new Date();
+
         try {
-          parsedData = JSON.parse(data);
-          console.log('StatusPage.ionViewDidEnter JSON.parse ', data);
+          let json = JSON.stringify(receivedData);
+          let bufferOriginal = Buffer.from(JSON.parse(json).data);
+          this.receivedData = bufferOriginal.toString('utf8');
+
+          console.log('StatusPage.ionViewDidEnter JSON.parse ', receivedData);
         } catch (error) {
           console.error('StatusPage.ionViewDidEnter JSON.parse ', error);
-        }
-
-        if (parsedData.hasOwnProperty('stt')) {
-          console.log('StatusPage.ionViewDidEnter JSON.parse valid', parsedData);
-          if (this.loading) {
-            await this.loading.dismiss();
-          }
-          this.data = parsedData;
-          this.date = new Date();
         }
       });
     });
@@ -101,12 +93,10 @@ export class StatusPage implements OnInit {
       );
     } else {
       // Connect
-      this.presentLoading(1000);
       this.serialPortService.connect(this.serialDevice).then(
         currentDevice => {
           this.serialDevice = currentDevice;
-          console.log('DevicesPage.toggleConnection', currentDevice.isConnected);
-          // this.serialDevice.isConnected = true;
+          this.serialPortService.startCommunication();
           this.presentToast('Connected to ' + this.serialDevice.name);
           console.log('DevicesPage.toggleConnection connect', this.serialDevice);
         },
@@ -118,72 +108,10 @@ export class StatusPage implements OnInit {
     }
   }
 
-  toggleCommunication() {
-    if (this.serialDevice && this.serialDevice.isConnected) {
-      if (this.serialDevice.isCommunicationOn) {
-        this.serialPortService.stopCommunication();
-        this.presentToast('Communication off');
-      } else {
-        // TODO: send date
-        this.presentLoading(5000);
-        this.serialPortService.startCommunication();
-        this.presentToast('Communication on');
-        this.refreshConsole();
-      }
-    } else {
-      this.presentAlert('Connect to a device first!');
-    }
-  }
-
-  toggleLog() {
-    if (this.serialDevice && this.serialDevice.isConnected) {
-      if (this.serialDevice.isCommunicationOn) {
-        if (this.data && this.data.stt.log) {
-          this.presentLoading(5000);
-          this.dataLoggerService.stopDataLogger().then(
-            () => {
-              this.presentToast('Command sent');
-            },
-            error => {
-              this.presentAlert('Problem sending command: ' + error);
-            }
-          );
-        } else {
-          this.presentLoading(5000);
-          this.dataLoggerService.startDataLogger().then(
-            () => {
-              this.presentToast('Command sent');
-            },
-            error => {
-              this.presentAlert('Problem sending command: ' + error);
-            }
-          );
-        }
-      } else {
-        this.presentAlert('Start communication first!');
-      }
-    } else {
-      this.presentAlert('Connect to a device first!');
-    }
-  }
-
-  async refreshConsole() {
-    if (this.serialDevice && this.serialDevice.isConnected) {
-      if (this.serialDevice.isCommunicationOn) {
-        this.dataLoggerService.readAllSensors().then(
-          () => {
-            this.presentToast('Command sent');
-          },
-          error => {
-            this.presentAlert('Problem sending command: ' + error);
-          }
-        );
-      } else {
-        this.presentAlert('Start communication first!');
-      }
-    } else {
-      this.presentAlert('Connect to a device first!');
-    }
+  sendData() {
+    this.serialPortService.write(this.sentData).then(() => {
+      this.presentToast('Data sent');
+    });
   }
 
   async presentToast(message: string) {
@@ -193,24 +121,5 @@ export class StatusPage implements OnInit {
       showCloseButton: true
     });
     await toast.present();
-  }
-
-  async presentAlert(message: string) {
-    const alert = await this.alertController.create({
-      header: 'Error!',
-      message: message,
-      buttons: ['OK']
-    });
-
-    await alert.present();
-  }
-
-  async presentLoading(time: number) {
-    this.loading = await this.loadingController.create({
-      message: 'Please wait...',
-      duration: time,
-      translucent: true
-    });
-    await this.loading.present();
   }
 }
